3e6b32a92911a988023f23119b9324ae3cdeaf95
Isaac Moreno <ipdxcreative@gmail.com>
2021-06-28 18:19:53 -0700

    :beers: monday follow along my dudes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ticketsimage from "./../img/ticketsimage.jpg"
+// import ticketsimage from "./../img/ticketsimage.jpg"
 
 function Header(){
   return (
   <React.Fragment>
     <h1>Help Queue</h1>
-    <img src={ticketsimage} alt="practice"/>
+    {/* <img src={ticketsimage} alt="practice"/> */}
   </React.Fragment>
   );
 }
