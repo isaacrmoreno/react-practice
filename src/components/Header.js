04a756342047a60310f916d0e97fe59c863ab702
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-13 19:17:55 -0700

    :lipstick: deployed site and added basic styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,33 @@
 // import ticketsimage from "./../img/ticketsimage.jpg"
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const HelpQueueHeader = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  color: black;
+`;
+
+const StyledWrapper = styled.section`
+  background-color: orange;
+`;
 
 function Header(){
   return (
-    <React.Fragment>
-      <h1> Help Queue</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/signin">Sign In</Link>
-        </li>
-      </ul>
-    </React.Fragment>
+    <StyledWrapper>
+          <HelpQueueHeader>
+            Help Queue
+          </HelpQueueHeader>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+          </ul>
+    </StyledWrapper>
   );
 }
 
